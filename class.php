f7384188122e93b8f4bc56ae1bd856008de1bb1d
RitaJ <ritaj777@gmail.com>
2018-10-26 10:02:03 +0300

    add new button

--- class.php
+++ class.php
@@ -1,14 +1,12 @@
 <?php
 
 echo "Create new class on master";
-<<<<<<< HEAD
 
 echo "Create new Book class on master";
 
 echo "Create new Book class methods on master";
 
 echo "Create new Book class constructor on master";
-||||||| merged common ancestors
-=======
 echo "Create new Animal class on dev2";
->>>>>>> div
+
+echo " working on master";
